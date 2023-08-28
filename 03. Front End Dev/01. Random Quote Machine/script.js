c0ef27f8819b2aed17ed3291256ade3b615addfb
kdy1994kko <101077732+kdy1994kko@users.noreply.github.com>
2023-08-27 22:31:09 -0600

    Add files via upload

--- 03. Front End Dev/01. Random Quote Machine/script.js
+++ 03. Front End Dev/01. Random Quote Machine/script.js
@@ -20,12 +20,13 @@ var currentQuote = '',
 
 function getQuotes() {
   return $.ajax({
-    headers: {
-      Accept: 'application/json'
+    headers: { 
+      Accept: 'application/json' // CHANGE LINK BELOW TO { 06. BIBLE VERSES } 
     },
     url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    //url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
     success: function (jsonQuotes) {
-      if (typeof jsonQuotes === 'string') {
+      if (typeof jsonQuotes === 'string') { // CHANGE LINK ABOVE TO { 06. BIBLE VERSES }  
         quotesData = JSON.parse(jsonQuotes);
         console.log('quotesData');
         console.log(quotesData);
@@ -33,7 +34,7 @@ function getQuotes() {
     }
   });
 }
-
+ 
 function getRandomQuote() {
   return quotesData.quotes[
     Math.floor(Math.random() * quotesData.quotes.length)
@@ -52,7 +53,7 @@ function getQuote() {
       encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
   );
 
-  $('#tumblr-quote').attr(
+  $('#tumblr-quote').attr( // AI == Change To FB or LinkedIn 
     'href',
     'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
       encodeURIComponent(currentAuthor) +
